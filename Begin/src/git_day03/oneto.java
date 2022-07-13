10fec85db0b48ddbf8376dfb02535e69e28da690
jihye <lullu303@naver.com>
2022-07-13 17:16:04 +0900

    one_two

---
+++ Begin/src/git_day03/oneto.java
@@ -0,0 +1,27 @@
+package git_day03;
+
+public class oneto {
+	public static void main(String[] args) {
+		int hap=0;
+		for(int i=1;i<101;i++) {
+			if((i%3==0) || (i%4==0))
+				hap = hap+i;
+				
+		}
+			System.out.println("3또는 4의 배수의 합: "+hap);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
