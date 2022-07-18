a8836f862dda9fe62a485884f4ae1363e2a71ec9
jihye <lullu303@naver.com>
2022-07-18 16:15:46 +0900

    day_11

--- Begin/src/day11/Outer.java
+++ Begin/src/day11/Outer.java
@@ -5,6 +5,14 @@ public class Outer {
 		   int a=1;
 	static int b=2;
 	
+	void print() {
+		Inner i=new Inner();
+		System.out.println(i.c);
+		
+		Outer.Inner oi=this.new Inner();
+		System.out.println(oi.c);
+	}
+	
 	class Inner{ //Inner Member class -non-static
 		int c=3;
 		void sub() {
