79601b9f52e800f604c580eb53ebd97054416593
jihye <lullu303@naver.com>
2022-07-18 17:23:44 +0900

    day___11

--- Begin/src/day11/MyGui.java
+++ Begin/src/day11/MyGui.java
@@ -1,6 +1,8 @@
 package day11;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+
 /* 이벤트 처리
  * 	[1] 이벤트 소스
  * 	[2] 이벤트
@@ -35,9 +37,41 @@ public class MyGui extends JFrame {
 		pN.add(bG);
 		pN.add(bB);
 		pN.add(bExit);
+		//생성자 안에서 이벤트소스와 이벤트 핸들러를 연결해야 함.
+		// - "이벤트소스.addXXXListener(이벤트핸들러객체)"
+		MyEventHandler handler=new MyEventHandler();
+		//MyGui.MyEventHandler handler=this.new.MyEventHandler();
+		bR.addActionListener(handler);
+		bG.addActionListener(handler);
+		bB.addActionListener(handler);
+		bExit.addActionListener(handler);
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+	}//생성자-----------------------------------------------
+	//이벤트 핸들러들은
+	//XXXListener상속받아야 함
+	class MyEventHandler implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			//이벤트가 발생된 객체의 참조값을 얻어보자.
+			Object obj=e.getSource();//이벤트 소스에 대한 정보를 반환함.
+			if(obj==bR) {
+			//ta.setBackground(Color.red); =>글자색
+			//이벤트 처리 코드
+			ta.setForeground(Color.red);
+			}else if(obj==bG) {
+				ta.setForeground(Color.green);
+			}else if(obj==bB) {
+				ta.setForeground(Color.blue);
+			}else if(obj==bExit){
+				System.exit(0);//시스템 종료
+				
+			}
+		}
+		
+	}////////////////////
+	
 
 	public static void main(String[] args) {
 		MyGui my=new MyGui();
