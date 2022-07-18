de321bc9e2c57422cad053fc89ba24c8616f87fb
jihye <lullu303@naver.com>
2022-07-18 20:50:56 +0900

    day_111

---
+++ Begin/src/day11/MySearchEngine.java
@@ -0,0 +1,68 @@
+package day11;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MySearchEngine extends JFrame {
+	
+	JPanel p=new JPanel();
+	JButton na, da, go, ya;
+	
+	
+	public MySearchEngine() {
+		super("::MySearchEngine::");
+		add(p, "Center");
+		
+		//p.setLayout(new BorderLayout());
+		
+		
+		na=new JButton("NAVER");
+		da=new JButton("DAUM");
+		go=new JButton("Google");
+		ya=new JButton("Yahoo");
+		
+		p.add(na, "NORTH");
+		p.add(da, "SOUTH");
+		p.add(go, "EAST");
+		p.add(ya, "WEST");
+		
+		
+		
+		//이벤트소스, 이벤트 핸들러 연결하기
+		MyEventHandler handler=new MyEventHandler();
+		
+		na.addActionListener(handler);
+		da.addActionListener(handler);
+		go.addActionListener(handler);
+		ya.addActionListener(handler);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}//생성자
+	class MyEventHandler implements ActionListener 
+	{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Object obj=e.getSource();
+			if(obj==na) {
+				na.setBackground(Color.green);
+			}else if(obj==da) {
+				da.setBackground(Color.cyan);
+			}else if(obj==go) {
+				go.setBackground(Color.yellow);
+			}else if(obj==ya) {
+				ya.setBackground(Color.magenta);
+			}
+		}
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		MySearchEngine my= new MySearchEngine();
+		my.setSize(500,500);
+		my.setVisible(true);
+		
+	}
+
+}
---
+++ JAVA_BOOK/src/DAY03/Person.java
@@ -0,0 +1,10 @@
+package DAY03;
+
+public class Person {
+	String name;
+	int height;
+	double weight;
+	char gender;
+	boolean married;
+
+}
