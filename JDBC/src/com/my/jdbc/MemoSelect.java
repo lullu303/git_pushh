6a495c7980e1f6bea5836e1fbb1ef68720082fcb
jihye <lullu303@naver.com>
2022-08-03 17:10:48 +0900

    memoselect1

---
+++ JDBC/src/com/my/jdbc/MemoSelect.java
@@ -0,0 +1,18 @@
+package com.my.jdbc;
+import java.sql.*;
+public class MemoSelect {
+
+	public static void main(String[] args)
+	throws Exception
+	{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		String url="jdbc:mysql://localhost:3306/schooldb?characterEncoding=UTF-8";
+		//프로토콜:dbms유형://host:port/database
+		String user="root", pwd="multi123";
+
+		Connection con=DriverManager.getConnection(url, user, pwd);
+		String sql="SELECT idx, name, msg, wdate FROM memo ORDER BY 1 DESC";
+
+	}
+
+}
